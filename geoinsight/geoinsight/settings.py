1ff185553bc4829739031d6396cc0be49ea5c76d
Abdullah ALwahdi <abdullahalwahdi464@gmail.com>
2024-09-25 19:04:58 +0000

    Configure Django to use MySQL

--- geoinsight/geoinsight/settings.py
+++ geoinsight/geoinsight/settings.py
@@ -74,11 +74,12 @@ WSGI_APPLICATION = "geoinsight.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
